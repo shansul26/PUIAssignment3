512281d33a20f503107bcef38de7317cbba77db0
Shannon <sullivsm@lafayette.edu>
2016-10-09 20:50:22 -0400

    Edit function
    Added the ability to edit the task name of a list item.

--- js/todolast.js
+++ js/todolast.js
@@ -8,30 +8,22 @@ function loadListItems(){
 }
 
 $("#add-task").click( function() {
-    createNewTask($("#task-name").val());
-    $("#task-name").val("");
+    createNewTask($("#add-task-name").val());
+    $("#add-task-name").val("");
     $("#assigned-to").val("");
     $("#add-item-modal").modal("hide");
 });
-
-/*
-TODO: delete this
-function addListItem(){
-    createNewTask($("#task-name").val());
-    $("#task-name").val("");
-    
-}*/
 
 function createNewTask(taskName){
     
     var taskDivId = "task" + nextTaskId;
     
     $("#tasks").append($("<div>").attr("class", "row list-item").attr("id", taskDivId)
-        .append($("<div>").attr("class", "col-xs-5 todo-item-content")
+        .append($("<div>").attr("class", "col-xs-6 todo-item-content")
             .append($("<input>").attr("type", "checkbox").attr("class", "todo-checkbox"))
             .append($("<span>").attr("class", "task-text").text(taskName)))
-        /*.append($("<button>").attr("class", "btn btn-default delete-button col-xs-1")
-                .append($("<span>").attr("class", "glyphicon glyphicon-trash")))*/
+        .append($("<button>").attr("class", "btn btn-default edit-button col-xs-1").attr("data-toggle", "modal").attr("data-target","#edit-item-modal")
+                .append($("<span>").attr("class", "glyphicon glyphicon-pencil")))
         .append($("<button>").attr("class", "btn btn-danger delete-button col-xs-1")
                 .append($("<span>").attr("class", "glyphicon glyphicon-trash"))));
      
@@ -57,21 +49,16 @@ $(document.body).on("change", ".todo-checkbox", function() {
             $("#tasks").append(row);
         }
 });
-/*$(document).ready(function() {
-    $(".delete-button").click(function(){
-        $(this).parent("div").remove();
-    });
-    $(".todo-checkbox").change(function(){
-        if($(this).is(":checked")){
-            //move to the "done" list
-            var row = $(this).parent("div").parent("div");
-            $("#completed-tasks").append(row);
-            
-        }
-        else{
-            //move to the "to-do" list
-            var row = $(this).parent("div").parent("div");
-            $("#tasks").append(row);
-        }
-    });
-});*/
+
+$(document.body).on("click", ".edit-button", function() {
+    var taskName = $(this).parent("div").children("div").children("span").text();
+    $("#edit-task-name").val(taskName);
+    $("#edit-task-name").attr("editing", $(this).parent("div").attr("id"));
+});
+
+$("#save-changes").click( function() {
+    var $updateId = "#" + $("#edit-task-name").attr("editing");
+    $($updateId).children("div").children("span").text($("#edit-task-name").val())
+    $("#edit-task-name").val("");
+    $("#edit-item-modal").modal("hide");
+});
